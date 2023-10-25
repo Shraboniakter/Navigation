82207f174cb2d01841c71d4c4eb97ff3f002206c
Shraboniakter <shrabonbhuiyan708@gmail.com>
2023-10-25 12:33:02 +0600

    This is Card

--- lib/main.dart
+++ lib/main.dart
@@ -21,60 +21,21 @@ class Myapp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Home'),),
       body: Center(
-        child: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder:(context)=>Page1()));
-            }, child:Text('Page1')),
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>Page2()));
-            }, child:Text('Page2')),
+        child:Card(
+          elevation: 30,
+   // margin: EdgeInsets.zero,
+    //clipBehavior: Clip.antiAlias,
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(16)),
+            child: SizedBox(
+          height: 100,
+          width: 300,
+          child:Center( child:Text('This is card')),
+        )
 
-        ],),
+          ),
       ),
-    );
-  }
-}
-class Page1 extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("page1"),),
-      body: Center(child: Text('Software Developer',style: TextStyle(fontWeight: FontWeight.bold),),),
-    );
-  }
-}
-class Page2 extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Page2"),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child:
-            Text('Hello Shrabon',style: TextStyle(fontWeight: FontWeight.bold),),),
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>Page3('shrabon')));
-          }, child: Text('Page3'))
-        ],
-      ),
-    );
-  }
-}
-class Page3 extends StatelessWidget {
-  String msg;
-   Page3(this.msg, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(msg),),
-      body: Center(child:
-      Text('Iam Try',style: TextStyle(fontWeight: FontWeight.bold),),),
     );
   }
 }
